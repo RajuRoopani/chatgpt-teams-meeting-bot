117600e5fd7a011ac8b817f63b5daf67c7a2ddab
rroopani <rroopani@microsoft.com>
2023-01-30 09:42:32 -0800

    adding graph helper

--- bot/config.ts
+++ bot/config.ts
@@ -2,6 +2,10 @@ const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
   openaiApiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.ORGANIZATION,
+  MicrosoftAppId: process.env.MicrosoftAppId,
+  MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+  MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 };
 
 export default config;
--- bot/teamsBot.ts
+++ bot/teamsBot.ts
@@ -4,11 +4,15 @@ import {
   TurnContext,
   AdaptiveCardInvokeValue,
   AdaptiveCardInvokeResponse,
+  TeamsInfo,
+  TeamsMeetingInfo
 } from "botbuilder";
 import rawWelcomeCard from "./adaptiveCards/welcome.json";
 import { AdaptiveCards } from "@microsoft/adaptivecards-tools";
 import { Configuration, OpenAIApi } from "openai";
 import config from "./config";
+import GraphHelper from "./helpers/graphHelper.js"
+
 
 export class TeamsBot extends TeamsActivityHandler {
   constructor() {
@@ -16,27 +20,59 @@ export class TeamsBot extends TeamsActivityHandler {
 
     const configuration = new Configuration({
       apiKey: config.openaiApiKey,
+      organization: config.organization
     });
+    
     const openai = new OpenAIApi(configuration);
 
     this.onMessage(async (context, next) => {
       console.log("Running with Message Activity.");
 
-      let txt = context.activity.text;
-      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
-      if (removedMentionText) {
-        // Remove the line break
-        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      const graphHelper = new GraphHelper();
+      if(context.activity.text.includes("summary")) {
+        const meetingDetails: any = await TeamsInfo.getMeetingInfo(context);
+        console.log(meetingDetails);
+        var result = await graphHelper.GetMeetingTranscriptionsAsync(meetingDetails.details.msGraphResourceId);
+        console.log(result);
+        if(result) {
+          let txt = "please create meeting summary for the following transcript?\n" + result;
+          const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+          if (removedMentionText) {
+            // Remove the line break
+            txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+          }
+          const openai = new OpenAIApi(configuration);
+    
+          const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: txt,
+            temperature: 0,
+            max_tokens: 2048,
+          });
+    
+          await context.sendActivity(response.data.choices[0].text);
+          
+        } else {
+          await context.sendActivity("No transcript available!");
+        }
+      } else {
+        let txt = context.activity.text;
+          const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+          if (removedMentionText) {
+            // Remove the line break
+            txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+          }
+          const openai = new OpenAIApi(configuration);
+    
+          const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: txt,
+            temperature: 0,
+            max_tokens: 2048,
+          });
+    
+          await context.sendActivity(response.data.choices[0].text);
       }
-
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: txt,
-        temperature: 0,
-        max_tokens: 2048,
-      });
-
-      await context.sendActivity(response.data.choices[0].text);
 
       // By calling next() you ensure that the next BotHandler is run.
       await next();
